2a6efcb9bd93c22b030d87f3cc0c7a079deab6c3
adiko <dikodam@googlemail.com>
2018-03-22 12:09:32 +0100

    abstractDay upgrade

--- src/main/java/de/dikodam/adventofcode/tools/AbstractDay.java
+++ src/main/java/de/dikodam/adventofcode/tools/AbstractDay.java
@@ -6,6 +6,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
 
@@ -15,13 +16,21 @@ public abstract class AbstractDay {
 
     public abstract void task2();
 
-    public List<String> getInput(String fileName) {
+    public Stream<String> streamInput(String fileName) {
         try (BufferedReader br = new BufferedReader(new FileReader(
             new File(getClass().getResource("/" + fileName).toURI())))) {
-            return br.lines().collect(toList());
+            return br.lines();
         } catch (IOException | URISyntaxException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public Stream<String> streamInput() {
+        return streamInput(getClass().getSimpleName());
+    }
+
+    public List<String> getInput(String fileName) {
+        return streamInput(fileName).collect(toList());
     }
 
     public List<String> getInput() {
