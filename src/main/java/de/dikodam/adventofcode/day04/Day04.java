4d56b9ce8313cae753d087cd1f435f4c88b3c285
adiko <dikodam@googlemail.com>
2018-03-23 14:40:37 +0100

    day 4 basics

---
+++ src/main/java/de/dikodam/adventofcode/day04/Day04.java
@@ -0,0 +1,20 @@
+package de.dikodam.adventofcode.day04;
+
+import de.dikodam.adventofcode.tools.AbstractDay;
+
+public class Day04 extends AbstractDay {
+
+    public static void main(String[] args) {
+        doTheMagic(Day04.class);
+    }
+
+    @Override
+    public void task1() {
+
+    }
+
+    @Override
+    public void task2() {
+
+    }
+}
