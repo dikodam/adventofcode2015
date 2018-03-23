69d93f72f38db8c09b927514ed3baa27a7dc0efb
adiko <dikodam@googlemail.com>
2018-03-23 11:45:44 +0100

    day02 task 1

--- src/main/java/de/dikodam/adventofcode/day02/Day02.java
+++ src/main/java/de/dikodam/adventofcode/day02/Day02.java
@@ -1,6 +1,7 @@
 package de.dikodam.adventofcode.day02;
 
 import de.dikodam.adventofcode.tools.AbstractDay;
+import de.dikodam.adventofcode.tools.Triple;
 
 public class Day02 extends AbstractDay {
 
@@ -8,12 +9,44 @@ public class Day02 extends AbstractDay {
         doTheMagic(Day02.class);
     }
 
-    public Day02 (){
+    // format: lxwxh
+    // surface: 2*l*w + 2*w*h + 2*h*l
+    // additional: smallest area
 
-    }
     @Override
     public void task1() {
+        long area = getInput()
+            .stream()
+            .map(this::parseAreas)
+            .mapToInt(this::computeWholeArea)
+            .sum();
+        System.out.println("area needed: " + area);
+    }
 
+    private int computeWholeArea(Triple<Integer, Integer, Integer> areaTriple) {
+        final int a = areaTriple.getA();
+        final int b = areaTriple.getB();
+        final int c = areaTriple.getC();
+        final int minArea = Math.min(a, Math.min(b, c));
+        return minArea + 2 * (a + b + c);
+    }
+
+    private Triple<Integer, Integer, Integer> parseAreas(String line) {
+        String[] dimensions = line.split("x");
+
+        final int length = Integer.parseInt(dimensions[0]);
+        final int width = Integer.parseInt(dimensions[1]);
+        final int height = Integer.parseInt(dimensions[2]);
+
+        final int area1 = length * width;
+        final int area2 = width * height;
+        final int area3 = height * length;
+
+        return new Triple<>(area1, area2, area3);
+    }
+
+    private int square(int i) {
+        return i * i;
     }
 
     @Override
--- src/main/java/de/dikodam/adventofcode/tools/AbstractDay.java
+++ src/main/java/de/dikodam/adventofcode/tools/AbstractDay.java
@@ -6,7 +6,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.List;
-import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
 
@@ -16,21 +15,13 @@ public abstract class AbstractDay {
 
     public abstract void task2();
 
-    public Stream<String> streamInput(String fileName) {
+    public List<String> getInput(String fileName) {
         try (BufferedReader br = new BufferedReader(new FileReader(
             new File(getClass().getResource("/" + fileName).toURI())))) {
-            return br.lines();
+            return br.lines().collect(toList());
         } catch (IOException | URISyntaxException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    public Stream<String> streamInput() {
-        return streamInput(getClass().getSimpleName());
-    }
-
-    public List<String> getInput(String fileName) {
-        return streamInput(fileName).collect(toList());
     }
 
     public List<String> getInput() {
