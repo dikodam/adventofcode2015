d561fb2c00f216827835e2b0be8e7d78d832a883
dikodam <adiko@gmx.de>
2020-11-06 23:10:44 +0100

    day 05 prep

---
+++ src/main/java/de/dikodam/adventofcode/day05/Day05.java
@@ -0,0 +1,17 @@
+package de.dikodam.adventofcode.day05;
+
+import de.dikodam.adventofcode.tools.AbstractDay;
+
+
+// classify strings by applying 3 predicates
+public class Day05 extends AbstractDay {
+    @Override
+    public void task1() {
+        // count
+    }
+
+    @Override
+    public void task2() {
+
+    }
+}
